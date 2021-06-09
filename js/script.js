52bd02bd6c288ef54fbfa55cd0192f6a7c0d563e
Springbank9 <isabellahay7@gmail.com>
2021-06-09 15:48:01 +0000

    going for a swim in the river

--- js/script.js
+++ js/script.js
@@ -1,26 +1,75 @@
-var $animation_elements = $('.animation-element');
-var $window = $(window);
-
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
-
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
-
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
+var video;
+function playVideo() {
+  console.log("playVideo called");
+  video.play();
 }
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+(function($){
+
+  
+  Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+      get: function(){
+          console.log(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+          return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+      }
+  })
+
+  // Once DOM is ready.
+  $(document).ready(function(){
+    video = document.getElementById("bg-video");
+    $('#bg-video').on('loadstart', function (event) {
+      $('#loader').fadeIn();
+    });
+
+    $('#bg-video').on('canplay', function(event) {
+      setTimeout(function() {
+        if (video.playing) {
+            $('#loader').fadeOut();
+        }
+
+        if (!video.playing) {
+            $('#loader').fadeOut();
+            if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+              $('#btn-play-bg').show();
+            }
+        }
+      }, 1000);
+    });
+
+    $('#btn-play-bg').on('click', function() {
+      $(this).hide();
+      playVideo();
+    });
+
+    if(!$('#container').hasClass('is-project')) {
+      return;
+    }
+  	var $quicklinks = $('.projects-quicklinks');
+  	var $quicklinksOffset = $quicklinks.offset().top;
+  	var $quicklinksHeight = $quicklinks.height();
+
+  	if (($quicklinksOffset + $quicklinksHeight) < $(window).height()) {
+  		$quicklinks.addClass('sticky');
+  	}
+
+  	$(document).on('scroll', function(e) {
+
+  		if ($(window).width() >= 768) {
+  			
+	  		var $scrollDistance = $(document).scrollTop();
+	  		var $windowHeight = $(window).height();
+	  		var visibleDistance = $windowHeight + $scrollDistance;
+	  		var quicklinksBottom = $quicklinksOffset + $quicklinksHeight;
+
+	  		if (visibleDistance >= quicklinksBottom  && !$quicklinks.hasClass('sticky')) {
+	  			$quicklinks.addClass('sticky');
+	  		} else if (visibleDistance < quicklinksBottom && $quicklinks.hasClass('sticky')) {
+	  			$quicklinks.removeClass('sticky');
+	  		}
+  		}
+  	});
+
+  });
+  
+
+}(jQuery));
